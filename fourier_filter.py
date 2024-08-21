2abf728970590c8e98453c6914bdfc30a0adff07
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2024-08-21 13:37:49 -0300

    Add docstrings

--- fourier_filter.py
+++ fourier_filter.py
@@ -1,13 +1,23 @@
 import numpy as np
+import pandas as pd
 
 
 class FourierFilter:
     def __init__(self, sample_rate, is_full_period=True):
+        '''
+        Instancializa um filtro de Fourier
+        Args:
+            sample_rate: taxa de amostragem do sinal de entrada
+            is_full_period: se é um filtro de período completo ou de meio período
+        '''
         self.sample_rate = sample_rate
         self.is_full_period = is_full_period
         self.create_filter()
 
     def create_filter(self):
+        '''
+            Cria os filtros cosseno e seno
+        '''
         if self.is_full_period:
             filter_args = np.linspace((2*np.pi / self.sample_rate), 2*np.pi, self.sample_rate)
             self.cosine_filter = ((2 / self.sample_rate) * np.cos(filter_args)).round(4)
@@ -20,12 +30,22 @@ class FourierFilter:
 
 class PhasorEstimator:
     def __init__(self, signal, sample_rate, is_full_period=True):
+        '''
+        Instancializa um estimador de fasor utilizando filtros de Fourier
+        Args:
+            signal: sinal de entrada
+            sample_rate: taxa de amostragem do sinal de entrada
+            is_full_period: se a estimação será feito com um filtro de período completo ou de meio período
+        '''
         self.signal = signal
         self.sample_rate = sample_rate
         self.is_full_period = is_full_period
         self.fourier_filter = FourierFilter(sample_rate, is_full_period)
 
     def estimate(self):
+        '''
+        Estima o fasor do sinal de entrada através da convolução do sinal com os filtros de Fourier
+        '''
         self.real_part = np.convolve(self.signal, self.fourier_filter.cosine_filter).round(4)
         self.imaginary_part = np.convolve(self.signal, self.fourier_filter.sine_filter).round(4)
         self.amplitude = np.sqrt(self.real_part**2 + self.imaginary_part**2).round(4)
@@ -33,10 +53,6 @@ class PhasorEstimator:
         self.phase = np.degrees(phase_rad).round(4)
 
 
-vc = np.array([-9.0745e1, 3.4725e0, 9.0736e1, -3.7006e0, -9.0727e1, 3.9286e0, 9.0717e1, -2.6706e0])
-phasor = PhasorEstimator(signal=vc, sample_rate=4, is_full_period=True)
+signal = pd.read_csv("./sinal.dat", delimiter='\t', names=['t', 'x'])
+phasor = PhasorEstimator(signal=signal['x'], sample_rate=12, is_full_period=True)
 phasor.estimate()
-print(f"Real Part: {phasor.real_part}")
-print(f"Imaginary Part: {phasor.imaginary_part}")
-print(f"Amplitude: {phasor.amplitude}")
-print(f"Phase: {phasor.phase}")
