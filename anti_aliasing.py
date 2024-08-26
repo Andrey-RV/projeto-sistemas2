267427f5144b1453f31f6c10fe1533392f226f68
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2024-08-26 16:51:53 -0300

    Change attribute name x_out to filtered_signal

--- anti_aliasing.py
+++ anti_aliasing.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-class AntiAliasing():
+class AntiAliasingFilter():
     def __init__(self, period, signal, b, c):
         self.signal = signal
         self.period = period
         self.a = 2 / self.period
         self.b = b
         self.c = c
-        self.filter()
+        self.apply_filter()
 
-    def filter(self):
+    def apply_filter(self):
         c0 = self.c / (self.a**2 + self.a*self.b + self.c)
         c1 = 2 * c0
         c2 = c0
@@ -18,10 +18,10 @@ class AntiAliasing():
         d2 = (self.a**2 + self.c - self.a*self.b) / (self.a**2 + self.a*self.b + self.c)
 
         num_samples = len(self.signal)
-        self.x_out = np.zeros((num_samples, 1))
+        self.filtered_signal = np.zeros((num_samples, 1))
 
-        self.x_out[0] = c0 * self.signal[0]
-        self.x_out[1] = c0 * self.signal[1] + c1 * self.signal[0] - d1 * self.x_out[0]
+        self.filtered_signal[0] = c0 * self.signal[0]
+        self.filtered_signal[1] = c0 * self.signal[1] + c1 * self.signal[0] - d1 * self.filtered_signal[0]
 
         for i in range(2, num_samples):
-            self.x_out[i] = c0 * self.signal[i-2] + c1 * self.signal[i-1] + c2 * self.signal[i] - d1 * self.x_out[i-1] - d2 * self.x_out[i-2]
+            self.filtered_signal[i] = c0 * self.signal[i-2] + c1 * self.signal[i-1] + c2 * self.signal[i] - d1 * self.filtered_signal[i-1] - d2 * self.filtered_signal[i-2]
