d1f563d39c233ff695b98101ea80e2731af20bd2
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2024-08-27 21:30:05 -0300

    Add docstrings

--- anti_aliasing.py
+++ anti_aliasing.py
@@ -3,6 +3,14 @@ import numpy as np
 
 class AntiAliasingFilter():
     def __init__(self, period, signal, b, c):
+        '''
+        Instancia um filtro de anti-aliasing.
+        Attributes:
+            signal: Uma sequência de amostras que compõe o sinal.
+            period: A diferença de tempo entre as amostras.
+            b: Parâmetro b
+            c: Parâmetro c
+        '''
         self.signal = signal
         self.period = period
         self.a = 2 / self.period
@@ -11,6 +19,10 @@ class AntiAliasingFilter():
         self.apply_filter()
 
     def apply_filter(self):
+        '''
+            Aplica o filtro anti-aliasing ao sinal de entrada via
+            xout(n) = [c0.xin(n) + c1.xin(n-1) + c2.xin(n-2)] - [d1.xout(n-1) + d2.xout(n-2)]
+        '''
         c0 = self.c / (self.a**2 + self.a*self.b + self.c)
         c1 = 2 * c0
         c2 = c0
