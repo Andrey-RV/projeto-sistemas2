cba7f932e980c68f6eb7de8019e0eca9af9e4d40
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2024-10-17 16:54:00 -0300

    Add 32F/N function

--- iec.py
+++ iec.py
@@ -3,7 +3,7 @@ from filters import AntiAliasingFilter, FourierFilter, MimicFilter
 
 
 class PhasorEstimator:
-    def __init__(self, signal, samples_per_cycle):
+    def __init__(self, signal, samples_per_cycle, num_points):
         '''
         Instancia um estimador de fasor.
         Attributes:
@@ -12,6 +12,7 @@ class PhasorEstimator:
         '''
         self.signal = signal.reshape(-1,)
         self.fourier_filter = FourierFilter(samples_per_cycle)
+        self.num_points = num_points
 
     def estimate(self):
         '''
@@ -19,10 +20,10 @@ class PhasorEstimator:
         '''
         self.real_part = np.convolve(self.signal, self.fourier_filter.cosine_filter)
         self.imaginary_part = np.convolve(self.signal, self.fourier_filter.sine_filter)
-        self.amplitude = np.sqrt(self.real_part**2 + self.imaginary_part**2)
-        phase_rad = np.arctan2(self.imaginary_part, self.real_part)
-        self.complex = self.amplitude * np.exp(1j * phase_rad)
+        self.amplitude = np.sqrt(self.real_part**2 + self.imaginary_part**2)[:self.num_points]
+        phase_rad = np.arctan2(self.imaginary_part, self.real_part)[:self.num_points]
         self.phase = np.degrees(phase_rad)
+        self.complex = self.amplitude * np.exp(1j * phase_rad)
 
 
 class Iec:
@@ -39,7 +40,7 @@ class Iec:
     }
 
     def __init__(self, va, vb, vc, ia, ib, ic, t, sampling_period, b, c, md,
-                 R, XL, phasor_estimator_samples_per_cycle, RTC, frequency=60):
+                 R, XL, estimator_samples_per_cycle, RTC, frequency=60):
         self.signals = {
             'va': va,
             'vb': vb,
@@ -55,7 +56,7 @@ class Iec:
         self.md = int(md)
         self.R = R
         self.XL = XL
-        self.phasor_estimator_samples_per_cycle = phasor_estimator_samples_per_cycle
+        self.estimator_samples_per_cycle = estimator_samples_per_cycle
         self.RTC = RTC
         self.frequency = frequency
         self.apply_anti_aliasing_filter()
@@ -85,34 +86,32 @@ class Iec:
 
     def estimate_phasors(self):
         self.phasors = {
-            'va': PhasorEstimator(self.signals['va'], self.phasor_estimator_samples_per_cycle),
-            'vb': PhasorEstimator(self.signals['vb'], self.phasor_estimator_samples_per_cycle),
-            'vc': PhasorEstimator(self.signals['vc'], self.phasor_estimator_samples_per_cycle),
-            'v0': PhasorEstimator(np.zeros_like(self.signals['va']), self.phasor_estimator_samples_per_cycle),
-            'ia': PhasorEstimator(self.signals['ia'], self.phasor_estimator_samples_per_cycle),
-            'ib': PhasorEstimator(self.signals['ib'], self.phasor_estimator_samples_per_cycle),
-            'ic': PhasorEstimator(self.signals['ic'], self.phasor_estimator_samples_per_cycle),
-            'i0': PhasorEstimator(np.zeros_like(self.signals['ia']), self.phasor_estimator_samples_per_cycle),
+            'va': PhasorEstimator(self.signals['va'], self.estimator_samples_per_cycle, len(self.time)),
+            'vb': PhasorEstimator(self.signals['vb'], self.estimator_samples_per_cycle, len(self.time)),
+            'vc': PhasorEstimator(self.signals['vc'], self.estimator_samples_per_cycle, len(self.time)),
+            'v0': PhasorEstimator(np.zeros_like(self.signals['va']), self.estimator_samples_per_cycle, len(self.time)),
+            'ia': PhasorEstimator(self.signals['ia'], self.estimator_samples_per_cycle, len(self.time)),
+            'ib': PhasorEstimator(self.signals['ib'], self.estimator_samples_per_cycle, len(self.time)),
+            'ic': PhasorEstimator(self.signals['ic'], self.estimator_samples_per_cycle, len(self.time)),
+            'i0': PhasorEstimator(np.zeros_like(self.signals['ia']), self.estimator_samples_per_cycle, len(self.time)),
         }
         for signal in self.phasors:
             if signal == 'v0':
                 self.phasors['v0'].amplitude = abs(
                     self.phasors['va'].complex + self.phasors['vb'].complex + self.phasors['vc'].complex
-                )[:len(self.time)]
+                )
                 self.phasors['v0'].phase = np.degrees(
                     np.angle(self.phasors['va'].complex + self.phasors['vb'].complex + self.phasors['vc'].complex)
-                )[len(self.time)]
+                )
             elif signal == 'i0':
                 self.phasors['i0'].amplitude = abs(
                     self.phasors['ia'].complex + self.phasors['ib'].complex + self.phasors['ic'].complex
-                )[:len(self.time)]
+                )
                 self.phasors['i0'].phase = np.degrees(
                     np.angle(self.phasors['ia'].complex + self.phasors['ib'].complex + self.phasors['ic'].complex)
-                )[len(self.time)]
+                )
             else:
                 self.phasors[signal].estimate()
-                self.phasors[signal].amplitude = self.phasors[signal].amplitude[:len(self.time)]
-                self.phasors[signal].phase = self.phasors[signal].phase[:len(self.time)]
 
     def add_51(self, type, gamma, adjust_current, curve):
         if type == 'phase':
@@ -155,3 +154,42 @@ class Iec:
             i_t[0][i_t[0] == adjust_current] = adjust_current + 0.01
             trip_time = np.where(i_t[0] <= adjust_current, np.inf, i_t[1])
             self.trip_time_50N = np.min(trip_time)
+
+    def add_32(self, type, alpha, beta):
+        if type == 'phase':
+            if alpha == 30:
+                pass
+            elif alpha == 60:
+                pass
+            elif alpha == 90:
+                v_pol_phases = {
+                    'a': np.degrees(np.angle(self.phasors['vb'].complex - self.phasors['vc'].complex)),
+                    'b': np.degrees(np.angle(self.phasors['vc'].complex - self.phasors['va'].complex)),
+                    'c': np.degrees(np.angle(self.phasors['va'].complex - self.phasors['vb'].complex)),
+                }
+                i_op_phases = {
+                    'a': self.phasors['ia'].phase,
+                    'b': self.phasors['ib'].phase,
+                    'c': self.phasors['ic'].phase,
+                }
+                op_region = {
+                    'a': (v_pol_phases['a'] - 90 + beta, v_pol_phases['a'] + 90 + beta),
+                    'b': (v_pol_phases['b'] - 90 + beta, v_pol_phases['b'] + 90 + beta),
+                    'c': (v_pol_phases['c'] - 90 + beta, v_pol_phases['c'] + 90 + beta),
+                }
+                self.trip_permission_32F = {
+                    'a': (i_op_phases['a'] > op_region['a'][0]) & (i_op_phases['a'] < op_region['a'][1]),
+                    'b': (i_op_phases['b'] > op_region['b'][0]) & (i_op_phases['b'] < op_region['b'][1]),
+                    'c': (i_op_phases['c'] > op_region['c'][0]) & (i_op_phases['c'] < op_region['c'][1]),
+                }
+
+        elif type == 'neutral':
+            if alpha == 30:
+                pass
+            elif alpha == 60:
+                pass
+            elif alpha == 90:
+                v_pol_phases = self.phasors['v0'].phase
+                i_op_phases = self.phasors['i0'].phase
+                op_region = (v_pol_phases - 90 + beta, v_pol_phases + 90 + beta)
+                self.trip_permission_32N = (i_op_phases >= op_region[0]) & (i_op_phases <= op_region[1])
