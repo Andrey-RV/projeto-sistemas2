aa832548e924b4b551a3ea2ad4a697c82cc9c922
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2025-01-22 20:57:16 -0300

    PhasorEstimator don't take the signal in its initialization. The phasor is now returned as complex type

--- phasor_estimator.py
+++ phasor_estimator.py
@@ -5,7 +5,7 @@ import numpy.typing as npt
 
 
 class PhasorEstimator:
-    def __init__(self, signal: npt.NDArray[np.float64], samples_per_cycle: int) -> None:
+    def __init__(self, samples_per_cycle: int) -> None:
         '''
         Instancia um estimador de fasor.
         Args:
@@ -15,42 +15,25 @@ class PhasorEstimator:
         Returns:
             None
         '''
-        self.__signal = np.array(signal)
         self.__samples_per_cycle = samples_per_cycle
         self.__fourier_filters = FourierFilter(self.__samples_per_cycle)
 
-    def __repr__(self, verbose: bool = False) -> str:
-        """Retorna uma representação do objeto em forma de string.
-
-        Args:
-            verbose (bool, optional): Seleciona se a representação conterá somente o shape e dtype do sinal ou parte do sinal em si. Defaults to False.
-
-        Returns:
-            str: Representação do objeto em forma de string.
-        """
-        if verbose:
-            signal_repr = f"signal={np.array2string(self.__signal, precision=3, threshold=5)}"
-        else:
-            signal_repr = f"signal=ndarray(shape={self.__signal.shape}, dtype={self.__signal.dtype})"
+    def __repr__(self) -> str:
         return (
             f"PhasorEstimator("
-            f"{signal_repr}, "
             f"samples_per_cycle={self.__samples_per_cycle}, "
-            f"amplitude={'set' if hasattr(self, 'amplitude') else 'unset'}, "
-            f"phase={'set' if hasattr(self, 'phase') else 'unset'})"
         )
 
-    def estimate(self) -> None:
+    def estimate(self, signal: npt.NDArray[np.float64]) -> complex:
         '''
-        Estima um fasor utilizando a convolução do sinal com os filtros de Fourier cosseno e seno.
+        Retorna a representação complexa do fasor estimado.
 
         Returns:
-            None
+            complex: O fasor estimado.
         '''
         self.__fourier_filters.create_filter()
-        real = fftconvolve(self.__signal, self.__fourier_filters.cosine_filter, mode='same')
-        imaginary = fftconvolve(self.__signal, self.__fourier_filters.sine_filter, mode='same')
-        rect_form = real + 1j * imaginary
-        self.amplitude = np.abs(rect_form)
-        self.phase = np.degrees(np.angle(rect_form))
-        self.exp_form = self.amplitude * np.exp(1j * np.radians(self.phase))
+        real = fftconvolve(signal, self.__fourier_filters.cosine_filter, mode='same')
+        imaginary = fftconvolve(signal, self.__fourier_filters.sine_filter, mode='same')
+        phasor = complex(real, imaginary)
+
+        return phasor
