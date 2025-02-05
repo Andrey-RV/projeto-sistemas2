f34ef26355ed13f2195281363f0e5eb81e5d9763
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2025-02-04 21:39:01 -0300

    Update docstrings
    Move the filter creation from estimate to __init__

--- phasor_estimator.py
+++ phasor_estimator.py
@@ -8,32 +8,30 @@ class PhasorEstimator:
     def __init__(self, samples_per_cycle: int) -> None:
         '''
         Instancia um estimador de fasor.
-        Args:
-            signal (npt.NDArray[np.float64]): o sinal a ser estimado.
-            samples_per_cycle (int): A quantidade de amostras capturadas pelo IED em um período da onda fundamental.
 
-        Returns:
-            None
+        Args:
+            samples_per_cycle (int): A quantidade de amostras capturadas pelo IED em um período da onda fundamental.
         '''
         self.__samples_per_cycle = samples_per_cycle
         self.__fourier_filters = FourierFilter(self.__samples_per_cycle)
+        self.__fourier_filters.create_filter()
 
     def __repr__(self) -> str:
-        return (
-            f"PhasorEstimator("
-            f"samples_per_cycle={self.__samples_per_cycle})"
-        )
+        return (f"PhasorEstimator(samples_per_cycle={self.__samples_per_cycle})")
 
     def estimate(self, signal: npt.NDArray[np.float64]) -> npt.NDArray[np.complex128]:
         '''
         Retorna a representação complexa do fasor estimado.
 
+        Args:
+            signal (npt.NDArray[np.float64]): O sinal base para a estimativa do fasor.
+
         Returns:
-            complex: O fasor estimado.
+            npt.NDArray[np.complex128]: O fasor estimado.
         '''
-        self.__fourier_filters.create_filter()
-        real = fftconvolve(signal, self.__fourier_filters.cosine_filter, mode='same')
-        imaginary = fftconvolve(signal, self.__fourier_filters.sine_filter, mode='same')
+
+        real = fftconvolve(signal, self.__fourier_filters.cosine_filter)[:len(signal)]
+        imaginary = fftconvolve(signal, self.__fourier_filters.sine_filter)[:len(signal)]
 
         phasor = real + 1j * imaginary
         return phasor
