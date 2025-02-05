a1fcd68e615611f50bb19311dee7a0a4d340eae1
Andrey R Vilaronga <andrey.rios@discente.univasf.edu.br>
2025-02-05 02:29:35 -0300

    Fix type hints

--- signals.py
+++ signals.py
@@ -3,14 +3,15 @@ import numpy.typing as npt
 from typing import Generator, TypeAlias
 from dataclasses import dataclass, field, fields
 
-name_value_pair: TypeAlias = tuple[str, npt.NDArray[np.complex128]]
+numeric_array: TypeAlias = npt.NDArray[np.float64 | np.complex128]
+name_value_tuple: TypeAlias = tuple[str, numeric_array]
 
 
 @dataclass
 class Signals:
     """Armazena um conjunto de sinais e tensões trifásicas, além do vetor de tempo e o período de amostragem.
 
-    Yields:
+    Attributes:
         va (npt.NDArray[np.complex128]): Tensão fase A.
         vb (npt.NDArray[np.complex128]): Tensão fase B.
         vc (npt.NDArray[np.complex128]): Tensão fase C.
@@ -33,36 +34,37 @@ class Signals:
         """Certifica-se de que todos os sinais estão armazenados como arrays numpy."""
         for field_ in fields(self):
             if field_.name != 'sampling_period':
-                setattr(self, field_.name, np.asarray(getattr(self, field_.name)))
+                value = getattr(self, field_.name)
+                setattr(self, field_.name, np.asarray(value))
 
-    def __iter__(self) -> Generator[tuple[str, npt.NDArray[np.float64]], None, None]:
+    def __iter__(self) -> Generator[tuple[str, numeric_array], None, None]:
         """Itera os atributos do objeto em forma de tuplas ('nome', valor).
 
         Returns:
-            Generator[tuple[str, npt.NDArray[np.float64 | np.complex128]], None, None]
+            Generator[tuple[str, numeric_array], None, None]
         """
         signals = [field_.name for field_ in fields(self) if field_.name not in ('t', 'sampling_period')]
         for signal in signals:
             yield signal, getattr(self, signal)
 
-    def __setitem__(self, key, value) -> None:
+    def __setitem__(self, key: str, value: numeric_array) -> None:
         setattr(self, key, value)
 
-    def __getitem__(self, key) -> npt.NDArray[np.float64]:
+    def __getitem__(self, key: str) -> numeric_array:
         return getattr(self, key)
 
-    def get_voltages(self) -> tuple[name_value_pair, name_value_pair, name_value_pair]:
-        """Retorna as tensões trifásicas do objeto.
+    def get_voltages(self) -> tuple[name_value_tuple, name_value_tuple, name_value_tuple]:
+        """Retorna as tensões de fase do objeto.
 
         Returns:
-            npt.NDArray[np.float64 | np.complex128]: Pares (name, value) das tensões trifásicas.
+            tuple[str, numeric_array]: Pares (name, value) das tensões de fase.
         """
         return ('va', self.va), ('vb', self.vb), ('vc', self.vc)
 
-    def get_currents(self) -> tuple[name_value_pair, name_value_pair, name_value_pair]:
-        """Retorna as correntes trifásicas do objeto.
+    def get_currents(self) -> tuple[name_value_tuple, name_value_tuple, name_value_tuple]:
+        """Retorna as correntes de fase do objeto.
 
         Returns:
-            npt.NDArray[np.float64 | np.complex128]: Pares (name, value) das correntes trifásicas.
+           tuple[str, numeric_array]: Pares (name, value) das correntes de fase.
         """
         return ('ia', self.ia), ('ib', self.ib), ('ic', self.ic)
